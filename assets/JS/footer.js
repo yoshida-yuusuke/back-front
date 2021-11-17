11136c86aa39d4e7a2398f19cf4c84c80f1f402c
QSW2019\JobTrain <jitianyoujie733@gmail.com>
2021-11-17 13:27:20 +0900

    assetsフォルダを追加

---
+++ assets/JS/footer.js
@@ -0,0 +1,12 @@
+
+//---------------------------
+// -----TOPへもどるボタン-----
+// --------------------------
+
+const PageTopBtn = document.getElementById("js-scroll-top");
+PageTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
