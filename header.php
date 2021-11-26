c0633f412707a4a0ca33fc188b409b80246d5c20
fujiokayumemi <fujiokayumemi@gmail.com>
2021-11-26 17:26:59 +0900

    todo 100 対応しました

--- header.php
+++ header.php
@@ -5,8 +5,13 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="author" content="hachisu">
+  <meta name="description" content="阿波うどん運動は徳島県特有の「なるちゅるうどん」
+「たらいうどん」を中心に徳島県の美味しいうどんを
+紹介するサイトです。あなたも徳島県で自分好みの
+うどんに出会ってください。">
   <title>阿波うどん運動</title>
-  <link rel="shortcut icon" href="favicon.ico">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/assets/img/favicon.ico">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/reset.css" />
   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/common.css" />
   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/css/header.css" />
