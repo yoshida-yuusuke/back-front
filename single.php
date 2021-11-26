7a8b5836085162ae49ac1d59bcc9bdb4a0dc878b
mormormk <arbre.mk@gmail.com>
2021-11-26 12:14:32 +0900

    特集記事詳細のレイアウト崩れを修正しました。single.phpとsingle-blog.cssの二つです。

--- single.php
+++ single.php
@@ -56,8 +56,6 @@
 
 
       <div class="cont-tag">
-        <!-- 画像のソース -->
-        <small class="cont-tag-txt">image:©️__________</small>
         <!-- タグ -->
         <small class="cont-tag-txt">
           <?php
@@ -93,7 +91,7 @@
   </section>
 
   <!----------------------
-       ----- おすすめ記事-----
+      ----- おすすめ記事-----
     ------------------------->
   <!-- おすすめ→recom -->
   <section class="recom-wrap">
@@ -111,7 +109,7 @@
         'post_type' => 'post',
         'paged' => $paged,
         'orderby' => 'rand',
-        'posts_per_page' => 5,
+        'posts_per_page' => 4,
       );
       $the_query = new WP_Query($args);
       if ($the_query->have_posts()) : ?>
@@ -119,47 +117,13 @@
         <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
           <?php $count++; ?>
           <div class="recom-article">
-            <?php if ($count = 5) {
-              get_template_part('template-parts/loop', 'speical5');
-            } else {
-              get_template_part('template-parts/loop', 'speical');
-            }; ?>
+            <?php get_template_part('template-parts/loop', 'speical'); ?>
           </div>
         <?php endwhile; ?>
         <?php wp_reset_postdata(); ?>
       <?php else : '検索結果がありませんでした'; ?>
       <?php endif; ?>
     </div>
-    <?php
-    //メニューの投稿タイプ
-    $args = array(
-      'post_type' => 'course',
-      'posts_per_page' => -1,
-    );
-    $the_query = new WP_Query($args);
-    if ($the_query->have_posts()) :
-      $count = 0;
-    ?>
-      <?php while ($the_query->have_posts()) : $the_query->the_post();
-        $count++;
-      ?>
-
-        <!-- 5番目だったら、archive-article-bigのクラスを付与する -->
-        <div class="archive-article<?php if ($count % 5 == 0) echo " archive-article-big"; ?>">
-          <div class="archive-article-img">
-            <a href="<?php the_permalink(); ?>">
-              <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="記事のサムネイル画像です" />
-            </a>
-            <!-- <button class="article-good">❤︎</button> -->
-          </div>
-          <a href="<?php the_permalink(); ?>" class="article-link">
-            <p class="article-title"><?php the_title(); ?></p>
-            <p class="article-txt"><?php the_excerpt(); ?></p>
-          </a>
-        </div>
-
-      <?php endwhile; ?>
-    <?php endif; ?>
     </div>
     <!-- 記事内容(recom-cont)終わり -->
   </section>
